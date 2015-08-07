6660f2bb1e25b449ca5ddc49fad83b00ff012dbb
Nate Meier <iemanatemire@gmail.com>
2015-08-06 19:50:06 -0700

    delete files

--- Server/server.js
+++
@@ -1,9 +0,0 @@
-// server.js -- initialize node.js server
-// ----------------------------------------------
-var app = require('./server-config.js');
-
-var port = 3000;
-
-app.listen(port);
-
-console.log('Beer-tab server listening on port ' + port);
