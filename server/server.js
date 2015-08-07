445ef347389f2f5fa05c9eedc59d52ce0c2b392e
Steven Wu <wucommasteven@gmail.com>
2015-08-07 12:52:39 -0700

    Include mongoose set up

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 // server.js -- initialize node.js server
 // ----------------------------------------------
 var app = require('./config/server-config.js');
-
 var port = 3000;
+var mongoose = require('mongoose');
+
+
+
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Mongodb Connection Open');
+});
+
 
 app.listen(port);
-
 console.log('Beer-tab server listening on port ' + port);
+
+module.exports = db;
