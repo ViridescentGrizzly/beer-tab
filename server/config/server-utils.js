ac1030dea505a8ea487e2ea718fad87766461a9a
Nate Meier <iemanatemire@gmail.com>
2015-08-07 12:36:46 -0700

    restructure server files

---
+++ server/config/server-utils.js
@@ -0,0 +1,30 @@
+// Helper functions for node server
+
+
+// ------------------------------------------
+//    Routing functions
+// ------------------------------------------
+exports. = function (req, res) {
+  res.redirect('/login');
+};
+
+// ------------------------------------------
+//    Authentication functions
+// ------------------------------------------
+exports.isLoggedIn = function(req, res) {
+  // Check if user is logged in
+};
+
+exports.checkUser = function (req, res, next) {
+  // Middleware to check if user is logged in before proceeding
+  if (!exports.isLoggedIn) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+
+};
+
+exports.logout = function (req, res) {
+  // Destroy user session
+};
