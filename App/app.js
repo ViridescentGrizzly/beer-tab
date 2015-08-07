ca006cd309d9cf093b6e49dadbee9d2bdb9af393
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-08-07 13:58:17 -0700

    Add auth module, ngRouter, build router in app.js

--- App/app.js
+++ App/app.js
@@ -1,1 +1,17 @@
-angular.module('app', []);
+angular.module('app', ['auth','ngRoute'])
+
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: './auth/login.html',
+        controller: 'AuthCtrl'
+      }) 
+      .when('/signup', {
+        templateUrl: './auth/signup.html',
+        controller: 'AuthCtrl'
+      })
+      .otherwise({
+        redirectTo: '/login'
+      });
+
+  }]);
