a52526556f81fca687333db82b306d031c018e11
Steven Wu <wucommasteven@gmail.com>
2015-08-11 13:41:28 -0700

    Initialize User Schema

--- server/server.js
+++ server/server.js
@@ -3,19 +3,38 @@
 var app = require('./config/server-config.js');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/beer-tab');
+mongoose.connect('mongodb://localhost:27017/beer-tab-db');
 
-var db = mongoose.connection;
+var schema = new mongoose.Schema ({
+                                    username: String, 
+                                    password: String,
+                                    network: mongoose.Schema.Types.Mixed
+                                  });
 
-db.on('error', console.error.bind(console, 'connection error:'));
+var User = mongoose.model('User', schema);
 
-db.once('open', function (callback) {
-  console.log('Mongodb Connection Open');
+var user1 = new User({
+                      username: 'argle', 
+                      password: 'bargle', 
+                      network: {
+                                'nathino': -3,
+                                'l337rofler': -2,
+                                'steven': 4
+                              }
+                    });
+
+user1.save(function(err, userObj){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('saved successfully: ', userObj);
+  }
 });
+
 
 var port = 3000;
 
 app.listen(port);
 console.log('Beer-tab server listening on port ' + port);
 
-module.exports = db;
+// module.exports = db;
