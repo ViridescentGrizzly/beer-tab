c918ea8f0401da6332190217c066bf20b1b64536
Nate Meier <iemanatemire@gmail.com>
2015-08-11 12:52:26 -0700

    add server routing for signup page

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,4 +1,19 @@
-angular.module('auth', [])
-  .controller('AuthCtrl', function () {
+angular.module('beer-tab.auth', [])
+  .controller('AuthCtrl', function ($scope, Auth) { // Come back here if shit doesn't work (look at $location)
+    $scope.user = {};
+
+    $scope.signup = function () {
+      console.log('Auth Controller');
+
+      Auth.signup($scope.user)
+        // .then(function (token) {
+        //   $window.localStorage.setItem('com.shortly', token);
+        //   $location.path('/links');
+        // })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
+
 
   });
