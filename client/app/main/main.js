4a3c57ea87685610925d0ce03789b9a91fcc9ae2
mKurrels <mjkurrels@gmail.com>
2015-08-12 16:14:21 -0700

    table works as far as I can tell

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,10 +1,16 @@
 angular.module('beer-tab.main', [])
+  .controller('MainCtrl', function ($scope, beerPmt) { // Come back here if shit doesn't work (look at $location)
+    $scope.users = [{name: "david", beers: -1},
+                    {name: "rosson", beers: 43},
+                    {name: "mark", beers: -4},
+                    {name: "bob", beers: 29},
+                    {name: "Enos", beers: 34}];
+    $scope.text = "beer paid!";
+    $scope.beered = function (user) {
+      beerPmt.recievePmt(user);
+      //.then(function () {
+        $scope.text = "sdf";
+      //});
 
-  .controller('MainCtrl', function ($scope) { // Come back here if shit doesn't work (look at $location)
-    $scope.users = [{name: "Moroni", age: 50},
-                    {name: "Tiancum", age: 43},
-                    {name: "Jacob", age: 27},
-                    {name: "Nephi", age: 29},
-                    {name: "Enos", age: 34}];
+    };
   });
-
